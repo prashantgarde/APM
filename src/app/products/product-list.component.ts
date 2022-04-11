7d44e4a913fd6f07858e928ddb52cece87d18c50
prashantgarde <prashantgarde5412@gmail.com>
2022-04-11 23:39:03 +0530

    preety formatted

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector : 'pm-products',
-    templateUrl : './product-list.component.html'
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
 
 })
-export class ProductListComponent
-{
+export class ProductListComponent {
     pageTitle: string = "Product List";
 }
