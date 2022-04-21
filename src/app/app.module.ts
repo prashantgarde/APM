b20804724d71a1bd7ecd0f09336b270dc22ce3c6
Prashant Garde <prashant.garde@FISGLOBAL.COM>
2022-04-21 08:01:50 +0530

    custom Pipe - changing "-" to " " in product code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-space.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces
   ],
   imports: [
     BrowserModule,
