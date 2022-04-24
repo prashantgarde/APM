deb49dd0f5d5f36a138db1c0156982c9c2fd009b
prashantgarde <42909511+prashantgarde@users.noreply.github.com>
2022-04-24 18:35:26 +0530

    Dell - More on Components (#2)
    * two way binding - making the filter tab take input from user
    
    * Transforming data with pipe-adding currency symbol
    
    * defining interfaces
    
    * unique CSS
    
    * lifecycle hook - OnInit
    
    * custom Pipe - changing "-" to " " in product code
    
    * getter and setters - listFilter property
    
    * filter product
    
    Co-authored-by: Prashant Garde <prashant.garde@********.COM>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpaces } from './shared/convert-to-space.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpaces
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
