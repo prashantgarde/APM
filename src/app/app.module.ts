cce29c65c3df16430802420cd5e350a18803c339
Prashant Garde <prashant.garde@FISGLOBAL.COM>
2022-04-24 20:08:47 +0530

    duplicate identifier removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpaces } from './shared/convert-to-space.pipe';
 import { PageTitleComponent } from './pageTitle.component';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpaces } from './shared/convert-to-space.pipe';
+
 
 
 @NgModule({
